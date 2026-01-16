01da688ba8f465d38cf5b1813e3dad016632dd45
shashi <shashiweb64@gmail.com>
2026-01-16 18:42:15 +0530

    cha

--- src/Service.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Service() {
-  return (
-   <div className="first"> this id JSX PAGE</div>
-  )
-}
-
-export default Service
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Xyz, {Mypage} from './About';
-import "./style.css";
-import Service from './Service';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-<Xyz></Xyz>
-<Mypage></Mypage>
-<Service></Service>
 
   </React.StrictMode>
 );
