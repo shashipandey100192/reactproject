8e7062ce8a22df96f0566f03f8afaad888a9f1ea
shashi <shashiweb64@gmail.com>
2026-01-23 19:00:02 +0530

    change

--- src/apps/modules/users/auth/User_login.js
+++ src/apps/modules/users/auth/User_login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import { MdOutlineEmail } from "react-icons/md";
 import { TbLockPassword } from "react-icons/tb";
 
 function User_login() {
+
+    const [xyz,abc] = useState(500);
+    const [one,two] = useState({name:"ravi",age:20,phone:"545454"});
+    const mymsg = ()=>{
+        abc("update value");
+        two({name:"mohan",age:80,phone:"777777777"})
+    }
+
+    const [a,b]=useState("username");
+
+    const userinput = (u)=>{
+        console.log(u);
+        b(u.target.value);
+    }
+
+
+
+const name="kumar";
+
   return (
    <div className="container">
     <div className="row justify-content-center">
@@ -11,12 +30,12 @@ function User_login() {
             <div className="container">
                 <div className="row">
                     <div className="col-12 text-center">
-                        <p className="h2">Sign In</p>
+                        <p className="h2">Sign In {name}</p>
                     </div>
                     <div className="col-md-12">
                         <div className="mb-3">
                             <label className="form-label"> <MdOutlineEmail/> Email address</label>
-                            <input type="email" className="form-control"/>
+                            <input type="email" className="form-control" value={a} onInput={userinput}/>
                         </div>
                     </div>
                     <div className="col-md-12">
@@ -31,6 +50,10 @@ function User_login() {
                             <input type="button" className="btn btn-success" value="login"/>
                             <input type="reset" className="btn btn-danger ms-2" value="cancel"/>
                             <Link to="registor">new user</Link>
+                        <p>{xyz}</p>
+                        <p>{one.age}</p>
+                        <input type='button' value="change" onClick={mymsg}/>
+
                         </div>
                     </div>
                 </div>
