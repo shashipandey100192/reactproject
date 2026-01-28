1ea6e4ff4b881257708621168eb2265b8a82037f
shashi <shashiweb64@gmail.com>
2026-01-28 18:35:25 +0530

    mainpage

--- src/apps/modules/dashboard/Mainpage.jsx
+++ src/apps/modules/dashboard/Mainpage.jsx
@@ -2,7 +2,92 @@ import React from 'react'
 
 function Mainpage() {
   return (
-    <div>Mainpage</div>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-3 mt-3'>
+          <div class="card text-bg-primary mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-md-8 mt-3'>
+          <div class="card mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-4 mt-3'>
+          <div class="card mb-3 shadow border">
+            <div class="card-body">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">card’s content.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
