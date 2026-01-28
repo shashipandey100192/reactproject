e5d5bdee1fb5bc7122e6d0b524b0da80d8c1b0f2
shashi <shashiweb64@gmail.com>
2026-01-28 18:19:49 +0530

    change

--- src/apps/modules/dashboard/Applandingpage.js
+++ src/apps/modules/dashboard/Applandingpage.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Appsidebar from '../shapremodules/Appsidebar'
+import Mainpage from './Mainpage'
+import Appheader from '../shapremodules/Appheader'
 
 function Applandingpage() {
   return (
-    <div>Applandingpage</div>
+    <Fragment>
+    <Appheader/>
+    <div className="container-fluid">
+      <div className='row'>
+        <div className='col-md-2 border'>
+          <Appsidebar/>
+        </div>
+        <div className='col-md-10 border'>
+          <Mainpage/>
+        </div>
+      </div>
+    </div>
+    </Fragment>
   )
 }
 
