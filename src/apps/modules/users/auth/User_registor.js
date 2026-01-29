27b061167fbe2ee134d2c46d230c655e2d484c82
shashi <shashiweb64@gmail.com>
2026-01-29 19:01:55 +0530

    validation

--- src/apps/modules/users/auth/User_registor.js
+++ src/apps/modules/users/auth/User_registor.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 function User_registor() {
+
+const {register,handleSubmit,formState: { errors }} = useForm()
+
+const formsubmit = (d)=>{
+    console.log(d);
+}
+
   return (
+    <form onSubmit={handleSubmit(formsubmit)}>
     <div className="container mt-3">
     <div className="row justify-content-center">
         <div className="col-md-7 bg-light shadow p-3">
@@ -14,32 +23,34 @@ function User_registor() {
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label">  Email address</label>
-                            <input type="email" className="form-control"/>
+                            <input type="email" className="form-control" {...register("emailid",{required:true})}/>
+                            {errors.emailid && <p className='text-danger'>email id is required</p>}
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label"> User Name</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" {...register("username",{required:true})}/>
+                            {errors.username && <p className='text-danger'>username is required</p>}
                         </div>
                     </div>
                         <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label"> DOB</label>
-                            <input type="date" className="form-control"/>
+                            <input type="date" className="form-control" {...register("dob")}/>
                         </div>
                     </div>
 
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label"> Mobile No</label>
-                            <input type="tel" className="form-control"/>
+                            <input type="tel" className="form-control" {...register("mobileno")}/>
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label">Gender</label>
-                            <select className='form-select'>
+                            <select className='form-select' {...register("gender")}>
                                 <option hidden>Gender</option>
                                 <option>Male</option>
                                 <option>Female</option>
@@ -50,7 +61,7 @@ function User_registor() {
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label"> Profile Pic</label>
-                            <input type="text" className="form-control" placeholder='url only'/>
+                            <input type="text" className="form-control" placeholder='url only' {...register("picture")}/>
                         </div>
                     </div>
 
@@ -58,13 +69,17 @@ function User_registor() {
                     <div className="col-md-6">
                         <div className="mb-3">
                             <label className="form-label"> Password</label>
-                            <input type="password" className="form-control"/>
+                            <input type="password" className="form-control" {...register("pass",{required:true,minLength:5,pattern:/^[0-9]{3}@[a-z]{3}$/})}/>
+                            {errors.pass?.type==="required" && <p className='text-danger'>password id is required</p>}
+                             {errors.pass?.type==="minLength" && <p className='text-info'>password minimun 5 charectors required</p>}
+                             {errors.pass?.type==="pattern" && <p className='text-warning'>statring 3 dit last 3 cha</p>}
+
                         </div>
                     </div>
 
                     <div className="col-md-12 text-center">
                         <div className="mb-3">
-                            <input type="button" className="btn btn-success" value="Registor Now"/>
+                            <input type="submit" className="btn btn-success" value="Registor Now"/>
                             <Link to="/usermanagement" className='ms-5'>login</Link>
                         </div>
                     </div>
@@ -74,6 +89,8 @@ function User_registor() {
         </div>
     </div>
 </div>
+</form>
+
   )
 }
 
